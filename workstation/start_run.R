235cff4fe978f02668d983fd7ae03bdc0076d14a
Christoph Hanauer <Christoph.Hanauer@uni-kassel.de>
2020-09-16 12:53:22 +0200

    Execute command on node in R

---
+++ workstation/start_run.R
@@ -0,0 +1,14 @@
+require('ssh')
+require('readr')
+
+start_run(pid) {
+	pid <- parse_number(pid)
+	print(pid)
+	ssh_exec_wait(session, command = paste('/pxe/meta/sim_start_on_nodes start ', node, sep="")
+}
+
+session <- ssh_connect("outsider@141.51.123.55")
+node <- 2
+
+ssh_exec_wait(session, command = paste('/pxe/meta/sim_start_on_nodes init ', node, sep=""), std_out = function(x) { start_run(rawToChar(x))})
+
