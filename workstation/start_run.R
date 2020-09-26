a55d64594be204bc5afc8d8aee48b8db29be07b1
Christoph Hanauer <christoph.hanauer@uni-kassel.de>
2020-09-26 15:39:21 +0200

    R example modified

--- workstation/start_run.R
+++ workstation/start_run.R
@@ -1,16 +1,14 @@
 require('ssh')
 require('readr')
 
-start_run <- function(pid) {
-	pid <- parse_number(pid)
-	print(pid)
+start_run <- function(cbstream) {
+	rpid <- rawToChar(cbstream)
+	pid <- parse_number(rpid)
 	Sys.sleep(5)
-	ssh_exec_wait(session, command = paste('BDEBUG=true /pxe/meta/sim_start_on_nodes start', node, pid, sep=" "))
-	ssh_disconnect(session)
+	ssh_exec_wait(session, command = paste('BDEBUG=true /pxe/meta/sim_start_on_nodes start 2', pid, sep=" "))
 }
 
 session <- ssh_connect("outsider@141.51.123.55")
-node <- 2
 
-ssh_exec_wait(session, command = paste('/pxe/meta/sim_start_on_nodes init ', node, sep=""), std_out = function(x) { start_run(rawToChar(x))})
+ssh_exec_wait(session, command = '/pxe/meta/sim_start_on_nodes init 2', std_out = function(x) { start_run(x)})
 
