b0f0dfd7ca35ba1adb03b6d8842a8484b10dd75c
Christoph Hanauer <christoph.hanauer@uni-kassel.de>
2020-10-13 15:47:45 +0200

    Correcting mistakes

--- workstation/R/example_run.R
+++ workstation/R/example_run.R
@@ -2,15 +2,15 @@ require('ssh')
 require('readr')
 
 # Full path to your simulation directory
-sim_path <- "~testsim"
+sim_path <- "/home/chh/ines-cluster/workstation/R/testsim"
 # Name of the simulation
 sim_name <- basename(sim_path)
 # Executable in simulation directory. Something like "java .." is possible too.
 sim_cmd <- "cmd"
 
 # Range of nodes that should run the simulation
-node_beg <- 2
-node_end <- 4
+node_beg <- 8
+node_end <- 10
 
 ############### Internals ###############
 
@@ -23,8 +23,11 @@ pxe <- "/pxe/meta/simulation/"
 # Init/Exec script on server node
 sim_ctrl <- "/pxe/meta/sim_start_on_nodes"
 
+# Distribution script on server node
+sim_dist <- "/pxe/meta/sim_to_nodes"
+
 # Current node we are working on used in global context
-current_node <- NULL
+current_node <- node_beg
 
 # Local simulation files
 sim_files <- dir(sim_path)
@@ -34,22 +37,25 @@ retrieve_pid <- function(cbstream) {
 	rpid <- rawToChar(cbstream)
 	pid <- parse_number(rpid)
 	Sys.sleep(2)
+	print(current_node)
 	start_sim(current_node, pid)
 	# return(pid)
 }
 
-start_sim <- function() {
-	upload_sim(files)
+start <- function() {
+	upload_sim(sim_files)
 	
 	for (node in node_beg:node_end) {
 		current_node <<- node
+		# assign("current_node", node, envir = .GlobalEnv)
 		ssh_exec_wait(session, command = paste(sim_ctrl, 'init', node, sep=" "), std_out = function(x) { retrieve_pid(x)})
 	}
 }
 
 # Starts the simulation
 start_sim <- function(node, pid) {
-	ssh_exec_wait(session, command = paste(sim_ctrl, 'start', node, pid, sep=" "))
+	print(paste("Starting on ", node))
+	ssh_exec_internal(session, command = paste(sim_ctrl, 'start', node, pid, sep=" "))
 }
 
 # Uploads simulation files to nodes
@@ -59,14 +65,19 @@ upload_sim <- function(files) {
 	out <- ssh_exec_wait(session, command = paste('mkdir', paste(pxe, sim_name, sep="/"), sep=" "))
 	
 	# Loop through node range and distribute
-	for (node in node_beg:node_end) {
-		for (f in files) {
-			out <- scp_upload(session, paste(sim_path, f, sep="/"), paste(pxe, sim_name, sep="/")) 
-		}
+	for (f in files) {
+		out <- scp_upload(session, paste(sim_path, f, sep="/"), paste(pxe, sim_name, sep="/")) 
 	}
+	
+	# Distribute to nodes
+	for (node in node_beg:node_end) {
+		out <- ssh_exec_wait(session, command = paste(sim_dist, sim_name, node, sep=" "))
+	}	
 }
 
-start_sim()
+# upload_sim(sim_files)
 
-ssh_discconect(session)
+start()
+
+ssh_disconnect(session)
 
